cde203599745ea372349817ddefadeb091e1fbfc
João Pedro <jpssam6@gmail.com>
2023-05-09 15:36:05 -0400

    add tests with strings

--- jest-activities/tests/words.test.js
+++ jest-activities/tests/words.test.js
@@ -6,4 +6,18 @@ describe('matchers expanded using words', () => {
     test('there is pool in liverpool', () => {
         expect('Liverpool').not.toMatch(/pol/)
     })
+
+    const typicalCuisine = [
+        "Tacacá",
+        "Tambaqui assado",
+        "Matrinxã na brasa",
+        "X-caboquinho",
+        "Pato no tucupi",
+        "Moqueca de pirarucu",
+        "Caldeirada de tambaqui"
+    ];
+
+    test('typical amazonense cuisine includes tacacá and tambaqui', () => {
+        expect(typicalCuisine).toContain('Tacacá', /tambaqui/);
+    })
 })
